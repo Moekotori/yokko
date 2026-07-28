6bcfb5806c6c168765e83a760153a28d6fbd8caf
Moekotori <nyafairy233@gmail.com>
2026-07-28 21:27:09 +0800

    Refine gameplay systems and application UI

--- Yokko.Game/Screens/Main/MainScreen.cs
+++ Yokko.Game/Screens/Main/MainScreen.cs
@@ -16,10 +16,11 @@ using osuTK;
 using osuTK.Graphics;
 using osuTK.Input;
 using Yokko.Audio;
+using Yokko.Core.Beatmaps;
 using Yokko.Game.Localisation;
 using Yokko.Game.Screens.Editor;
+using Yokko.Game.Screens.Gameplay;
 using Yokko.Game.Screens.Settings;
-using Yokko.Game.Screens.SongSelect;
 
 namespace Yokko.Game.Screens.Main;
 
@@ -557,7 +558,7 @@ public partial class MainScreen : Screen
                 YokkoStrings.Get("main.play"),
                 YokkoStrings.Get("main.song_select"),
                 FontAwesome.Solid.Play,
-                () => this.Push(new SongSelectScreen()))
+                () => this.Push(new GameplayScreen(DemoBeatmaps.CreateFourKeyDemo())))
             {
                 Y = 162,
             },
